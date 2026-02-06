f7e0af2d9ccafdfd1e8f496c38234b4108ffcee6
prerana <preranaroshan2007@gmail.com>
2026-02-06 19:26:15 +0800

    added /index.html route

--- app.py
+++ app.py
@@ -20,6 +20,10 @@ def get_db_connection():
 def home():
     return render_template("index.html")
 
+@app.route("/index.html")
+def index():
+    return render_template("index.html")
+
 @app.route("/info.html")
 def info():
     return render_template("info.html")
